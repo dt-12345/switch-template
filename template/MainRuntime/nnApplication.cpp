b95c022d7547cb11637535ddcf3b3978b05374f1
dt-12345 <damagetesting@proton.me>
2026-09-13 21:07:13 -0400

    don't think this needs to be exported

--- template/MainRuntime/nnApplication.cpp
+++ template/MainRuntime/nnApplication.cpp
@@ -1,6 +1,6 @@
 extern "C"
 {
-    __attribute__((visibility("default"))) void __nnDetailNintendoSdkNsoFileRefer();
+    void __nnDetailNintendoSdkNsoFileRefer();
     // this function is oddly placed at offset 0 and is entirely empty? not sure what's going on with it
-    __attribute__((visibility("default"))) [[gnu::naked]] void __nnDetailNintendoSdkNsoFileRefer() {}
+    [[gnu::naked]] void __nnDetailNintendoSdkNsoFileRefer() {}
 }
